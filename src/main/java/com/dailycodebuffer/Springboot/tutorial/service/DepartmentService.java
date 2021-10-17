3aa49f7a014cfa2495277ed2aa9dadfdb40af08b
clemens2206 <clekerber@tsn.at>
2021-10-17 12:48:39 +0200

    saveDepartment

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.dailycodebuffer.Springboot.tutorial.service;
 
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
 }
