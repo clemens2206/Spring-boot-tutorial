a6d8b7199e330c4ade6d2ad63315dac983e0d215
clemens2206 <clekerber@tsn.at>
2021-10-20 12:22:14 +0200

    spring-boot-starter-validation

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -1,9 +1,13 @@
 package com.dailycodebuffer.Springboot.tutorial.entity;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @Entity
 public class Department {
@@ -11,6 +15,18 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
+    /*@Length(max = 5, min = 1)
+    @Size(max = 10, min = 0)
+    @Email
+    @Positive
+    @Negative
+    @PositiveOrZero
+    @NegativeOrZero
+    @Future
+    @Past
+    @PastOrPresent*/
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
