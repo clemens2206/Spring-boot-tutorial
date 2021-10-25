31ffcdb026c72d9039cfc1d8c9e5d3414ea306d1
clemens2206 <clekerber@tsn.at>
2021-10-25 19:06:02 +0200

    Welcome Message

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String helloWorld(){
-        return "Welcome too Daily Code Buffer!!";
+        return welcomeMessage;
     }
 }
+
